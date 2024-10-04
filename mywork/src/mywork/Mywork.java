a0e8db46abee92e1b6fd9b70f4be0846f123c4fa
mutyaba01 <mutyabaedwin788@gmail.com>
2024-10-04 17:37:14 +0300

    Add files via upload

---
+++ mywork/src/mywork/Mywork.java
@@ -0,0 +1,17 @@
+package mywork;
+
+public class Mywork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Even numbers between 1 and 20:");
+for (int i=1; i<=20; i++)
+{
+	if (i % 2 == 0) {
+		System.out.println(i);
+	}
+	
+}
+	}
+
+}
